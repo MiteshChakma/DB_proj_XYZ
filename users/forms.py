e4459dec560417612dbe60f60b3ec1d3529d55a1
amin hasanzadeh <iqson.amin@gmail.com>
2023-12-09 18:15:13 +0200

    login and signup completed

--- users/forms.py
+++ users/forms.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 class CustomUserCreationForm(UserCreationForm):
     # Add additional fields as required
-    location = forms.CharField(required=False)
+    location = forms.CharField(required=True)
 
     class Meta(UserCreationForm.Meta):
         model = User
@@ -16,6 +16,33 @@ class CustomUserCreationForm(UserCreationForm):
     def save(self, commit=True):
         user = super().save(commit=False)
         user.location = self.cleaned_data['location']
+        user.role = User.CUSTOMER
+        status = ''
         if commit:
-            user.save()
-        return user
+            if(user.location=='finland'):
+                userExistency = list(User.objects.using('finland').filter(username=user.username).values())
+                if(userExistency==[]):
+                    userExistency = list(User.objects.using('sweden').filter(username=user.username).values())
+                    if(userExistency==[]):
+                        user.save(using=user.location)
+                        user = list(User.objects.using(user.location).filter(username = user.username).values())[0]
+                        del user['date_joined']
+                        del user['last_login']
+                    else:
+                        status = 'A user with that username already exists'
+                else:
+                    status = 'A user with that username already exists'
+            elif(user.location=='sweden'):
+                userExistency = list(User.objects.using('sweden').filter(username=user.username).values())
+                if(userExistency==[]):
+                    userExistency = list(User.objects.using('finland').filter(username=user.username).values())
+                    if(userExistency==[]):
+                        user.save(using=user.location)
+                        user = list(User.objects.using(user.location).filter(username = user.username).values())[0]
+                        del user['date_joined']
+                        del user['last_login']
+                    else:
+                        status = 'A user with that username already exists'
+                else:
+                    status = 'A user with that username already exists'
+        return user,status       
--- users/views.py
+++ users/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import User  
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+import json
+from django.forms.models import model_to_dict
 # Import your User model
 
 # Helper function to check roles
@@ -40,31 +43,38 @@ def customer_service_dashboard(request):
 def dba_dashboard(request):
     return render(request, 'dba_dashboard.html')
 
+def customAuth(username,password):
+    try:
+        user = User.objects.using('finland').get(username = username)
+        if(user.check_password(password)):
+            return user
+        return 'password is incorrect'
+    except User.DoesNotExist:
+        try:
+            user = User.objects.using('sweden').get(username = username)
+            if(user.check_password(password)):
+                return user
+            return 'password is incorrect'
+        except User.DoesNotExist:
+            return None
+
 # Login View
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            auth_login(request, user)
-
-            if user.is_administrator():
-                return redirect('admin_dashboard')
-            elif user.is_customer():
-                return redirect('customer_dashboard')
-            elif user.is_customer_service():
-                return redirect('customer_service_dashboard')
-            elif user.is_db_administrator():
-                return redirect('dba_dashboard')
-            else:
-                return redirect('default_page')
+        res= customAuth(username,password)
+        if res == 'password is incorrect':
+            return HttpResponse(json.dumps({'stu':0,'message': 'Password is incorrect'}))
+        if res == None:
+            return HttpResponse(json.dumps({'stu':0,'message': 'User is not existed'}))
         else:
-            # Handle invalid login here
-            pass
-
-    return render(request, 'login.html')
+            dictObj = model_to_dict(res)
+            del dictObj['date_joined']
+            del dictObj['last_login']
+            user = json.dumps(dictObj)
+            return HttpResponse(json.dumps({'stu':1,'message': user}))
+    return HttpResponse(json.dumps({'stu':0,'message': 'Server error'}))
 
 # Register View
 from .forms import CustomUserCreationForm
@@ -74,20 +84,13 @@ def register_view(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-
-            # Assign a role to the user if necessary
-            user.role = User.CUSTOMER  # For example
-            user.save()
-
-            auth_login(request, user)
-
-            # Redirect based on user role
-            return redirect('appropriate_dashboard')
-    else:
-        form = CustomUserCreationForm()
-
-    return render(request, 'register.html', {'form': form})
+            user,status = form.save()
+            if(status==''):
+                return HttpResponse(json.dumps({'stu':1,'message': user}))
+            return HttpResponse(json.dumps({'stu':0,'message': status}))
+        else:
+            return HttpResponse(json.dumps({'stu':0,'message': 'A user with that username already exists'}))
+    return HttpResponse(json.dumps({'stu':0,'message': 'Server error'}))
 
 
 
